480aff2f6abbf9ad18594f62f9a1d3e391d57ca7
montezuma15 <maria.montezuma@globant.com>
2026-01-21 18:50:14 -0500

    Fix arquitecture and create entity

---
+++ hotelcalifornia/src/main/java/com/hotel/domain/model/entity/Bed.java
@@ -0,0 +1,5 @@
+package com.hotel.domain.model.entity;
+
+public class Bed {
+
+}
---
+++ hotelcalifornia/src/main/java/com/hotel/domain/model/entity/Room.java
@@ -0,0 +1,12 @@
+package com.hotel.domain.model.entity;
+
+public class Room {
+    private String roomId;
+    private int roomNumber;
+    private float priceNight;
+    private int maxPeople;
+    private boolean availability;
+    private String roomTypeId;
+    private String statusId;
+
+}
